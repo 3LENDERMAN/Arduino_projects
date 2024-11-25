7a710a80ed94a15e76b9a51419faae7ff6c13436
Jakub Holub <121501421+3LENDERMAN@users.noreply.github.com>
2024-11-25 16:37:47 +0100

    Add files via upload

---
+++ 26_ESPNOW_Transmission.ino
@@ -0,0 +1,41 @@
+#include <ESP8266WiFi.h>
+#include <espnow.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS D2
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+uint8_t receiverMAC[] = {0x48, 0x55, 0x19, 0xec, 0xbe, 0x9e};
+
+// Callback pro odeslání dat
+void onDataSent(uint8_t *mac_addr, uint8_t sendStatus) {
+  Serial.print("Odesílání status: ");
+  Serial.println(sendStatus == 0 ? "Úspěšné" : "Neúspěšné");
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  WiFi.mode(WIFI_STA);
+  sensors.begin();
+  
+  if (esp_now_init() != 0) {   // Inicializace ESP-NOW
+    Serial.println("Chyba při inicializaci ESP-NOW");
+    return;
+  }
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
+  esp_now_register_send_cb(onDataSent);
+  esp_now_add_peer(receiverMAC, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+}
+
+void loop() {
+  sensors.requestTemperatures();
+  float temperatureC = sensors.getTempCByIndex(0); // Get temperature
+  
+  uint8_t data[sizeof(float)];
+  memcpy(data, &temperatureC, sizeof(temperatureC));
+  esp_now_send(receiverMAC, data, sizeof(data));
+  delay(10000);
+}
